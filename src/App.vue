36a4204f88ea8051ddf6d41f67d0c6a99964c8f0
King Star <mcxin.y@gmail.com>
2022-03-31 13:18:25 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -59,7 +59,7 @@ export default {
   -webkit-background-size: cover;
   -o-background-size: cover;
   background-position: center 0;
-  background-image: url(https://cdn.jsdelivr.net/gh/mcXinEra/Resource/JS/bkg-blur.png);
+  background-image: url(https://api.jszw.ml/CDN/Blog/JS/bkg-blur.png);
   opacity: 0.20;
 }
 
